9db5ca5bfc68e80f260b82ef09f609201a114b7e
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-03-17 16:39:17 +0900

    ユーザ認証の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
+  get 'mypage' => 'users#me'
+  post 'login' => 'users#create'
+  delete 'login' => 'usersd#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resources :users
   resources :boards
   resources :comments, only: %i[create destroy]
 end
